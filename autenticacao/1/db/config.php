a984fb6284550b1929ce727207fb9ad39591ae64
ce-a11y <110133088+ce-a11y@users.noreply.github.com>
2022-08-19 15:03:13 -0300

    Update config.php

--- autenticacao/1/db/config.php
+++ autenticacao/1/db/config.php
@@ -2,6 +2,11 @@
 error_reporting(0);
 session_start();
 
+// Define o modo que o servidor vai operar
+// No caso, o impacto disso aqui no projeto seriam só
+// os dados pra logar no banco de dados
+// e o negócio de enviar o e-mail pra confirmar mesmo
+
 $modo = 'producao';
 
 if ($modo == 'local') {
@@ -21,6 +26,7 @@ if ($modo == 'local') {
 
 }
 
+// Aqui tenta se conectar ao banco de dados caso consiga blz, caso n retorna um erro
 try {
     $pdo = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -28,11 +34,11 @@ try {
     echo "Falha ao se conectar com o banco!";
 }
 
-
+// Aqui é a função de "limpar" os campos lá
 function limpar($dados) {
-    $dados = trim($dados);
-    $dados = stripslashes($dados);
-    $dados = htmlspecialchars($dados);
+    $dados = trim($dados); // Retira espaços do começo e do fim de uma string
+    $dados = stripslashes($dados); // Retira barras invertidas de uma string, por exemplo: \' vira '
+    $dados = htmlspecialchars($dados); // Transforma caracteres especiais (tipo < > / ') em texto normal, previnindo inserção de scripts, por exemplo
     return $dados;
 }
 
